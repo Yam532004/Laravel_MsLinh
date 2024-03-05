4da9d91b55178e5bb2b7d0b8d1adc77926228f25
Yam532004 <yam532004@gmail.com>
2024-03-05 11:30:51 +0700

    Section

---
+++ app/Http/Controllers/SlideController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Slide;
+use Illuminate\Http\Request;
+
+class SlideController extends Controller
+{
+    public function index(){
+        $slides = Slide::all();
+        return view('slides',['slides' => $slides] );
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Models\Product;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\SlideController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/products', [ProductController::class, 'index']);
+
+Route::get('/products/slides', [SlideController::class. 'index']);
+Route::get('/products/{id} ', [ProductController::class, 'show'])->name('product/detail');
